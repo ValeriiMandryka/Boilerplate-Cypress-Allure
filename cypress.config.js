0062748dedea8821b17c34ecd22f5d9cafec6903
Valerii <mandryka.valerii@gmail.com>
2022-10-19 16:54:00 +0300

    modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     video:false,
     experimentalStudio: true,
+    baseUrl: 'https://example.cypress.io/todo',
     reporter: 'mocha-allure-reporter',
     specPattern: 'cypress/e2e/tests/*.js',
     setupNodeEvents(on, config) {
